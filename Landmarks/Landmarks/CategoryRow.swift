93d083bfd476f2071c22bd969ebd13471306f841
Misaki Shimazaki <msk.toybox@gmail.com>
2020-07-03 14:45:37 +0900

    Section3

---
+++ Landmarks/Landmarks/CategoryRow.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Misaki Shimazaki on 2020/07/03.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
