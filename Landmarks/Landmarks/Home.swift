d2d8a9fb8a33a285c66656e985be5a0bb5766e82
Misaki Shimazaki <msk.toybox@gmail.com>
2020-07-03 14:30:34 +0900

    Section2

---
+++ Landmarks/Landmarks/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  Landmarks
+//
+//  Created by Misaki Shimazaki on 2020/07/03.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
